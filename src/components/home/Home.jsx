214b43489eff53e48695bc1319a47f3f8ad9bebb
LuisUlloaC <luis.ulloa75360@gmail.com>
2023-11-14 11:13:27 -0400

    Created storie-card and stories-layout (need revision for responsiveness)

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import MiniDrawer from '../utils/drawer';
 import FeedCard from '../utils/feedCard';
+import StorieCard from '../utils/storiesCard';
 
 
 
@@ -12,8 +13,9 @@ export default function HomeScreen() {
     <div className='homeContainer'>
       <MiniDrawer />
       <div className='switchScreen'>
-        <FeedCard loading={true}/>
-
+      </div>
+      <div className='stories-layout'>
+        <StorieCard loading={true}/>
       </div>
     </div>
   );
--- src/components/utils/storiesCard.jsx
+++ src/components/utils/storiesCard.jsx
@@ -8,35 +8,35 @@ import colors from '../../colors';
 
 
 export default function StorieCard({loading}) {
-  return (
-    <Card className='article-card' sx={{display: 'flex',width: '25vw',  m: 2, backgroundColor: colors.feed_darker_purple }}>
-      {loading ? (
-        <Skeleton sx={{ height: 190 }} animation="wave" variant="rectangular" />
-      ) : (
-        <CardMedia
-          component="img"
-          height='62%'
-          image="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512"
-          alt="Nicola Sturgeon on a TED talk stage"
-        >
-        </CardMedia>
-      )}
-
-      <CardContent>
-        {loading ? (
-          <React.Fragment>
-            <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
-            <Skeleton animation="wave" height={10} width="80%" />
-          </React.Fragment>
-        ) : (
-          <Typography variant="body2" color="text.secondary" component="p">
-            {
-              "Why First Minister of Scotland Nicola Sturgeon thinks GDP is the wrong measure of a country's success:"
-            }
-          </Typography>
-        )}
-      </CardContent>
-    </Card>
-    
-  );
+  if (loading) {
+    return (
+      <div className="storie">
+        <div className="card-image">
+          <div className="title pulsate"></div>
+          <div className="storie-content">
+            <div className="content-title pulsate" />
+            <div className="content-subtitle pulsate" />
+            <div className="footer">
+              <div className="circle pulsate" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="card">
+        <div className="card-image">
+          waos
+          <div className="block pulsate"></div>
+        </div>
+        <div className="card-content">
+          <div className="block3 pulsate"></div>
+          <div className="block2 pulsate"></div>
+          <div className="circle pulsate"></div>
+          <div style={{ clear: "both" }}></div>
+        </div>
+      </div>
+    );
+  }
 }
