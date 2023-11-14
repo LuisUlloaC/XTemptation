0bb0afbc37fc2cfe87b1cec09172f7b438248708
LuisUlloaC <luis.ulloa75360@gmail.com>
2023-11-14 07:29:30 -0500

    Styling card feed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,25 +1,20 @@
 import * as React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import { ListItem } from '@mui/material';
-import Test from '../utils/feedCard';
 import MiniDrawer from '../utils/drawer';
-import colors from '../../colors';
 import FeedCard from '../utils/feedCard';
-import StorieCard from '../utils/storiesCard';
 
-const defaultTheme = createTheme();
+
 
 export default function HomeScreen() {
   const [loading, setLoading] = React.useState(true);
+  const [currentScreen, setCurrentScreen] = React.useState('home')
 
   return (
     <div className='homeContainer'>
       <MiniDrawer />
+      <div className='switchScreen'>
+        <FeedCard loading={true}/>
+
+      </div>
     </div>
   );
 }
