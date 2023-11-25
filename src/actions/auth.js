4718fdbc0087401f3789da9dd46e1fdc7d7e6293
LuisUlloaC <luis.ulloa75360@gmail.com>
2023-11-24 19:10:57 -0500

    The signIn action is no created

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,43 +1,60 @@
+export const userLogin = async (api, email, password) => {
+  let result = null;
+  let state_data = {};
+  let sucess = false;
 
-export const userLogin = async (api, email, password) => { 
-    let result = null;
-    let state_data = {};
-    let sucess = false;
+  await api
+    .post("/accounts/login/", {
+      email: email,
+      password: password,
+    })
+    .then((res) => {
+      state_data = {
+        access: res.data.access,
+        refresh: res.data.refresh,
+        profile_picture: res.data.profile_picture,
+        user_id: res.data.id,
+      };
+      result = res;
+      sucess = true;
+    })
+    .catch((res) => {
+      sucess = false;
+      result = res;
+    });
 
-    await api.post('/accounts/login/', {
-        email: email,
-        password: password})
-    .then(res => {
-        state_data = {
-            access: res.data.access,
-            refresh: res.data.refresh,
-            profile_picture: res.data.profile_picture,
-            user_id: res.data.id
-            };
-        result = res;
-        sucess = true })
-    .catch(res => {
-        sucess = false;
-            result = res; 
-          })
-
-    return (
-        {
-            result: result,
-            sucess: sucess,
-            state_data: state_data
-        }
-    )
-}
+  return {
+    result: result,
+    sucess: sucess,
+    state_data: state_data,
+  };
+};
 
 export const userSignIn = async (
-    api, user_name, email, profile_picture, banner_picture, is_active, is_staff, is_content_creator, is_escourt
+    api, user_name, email, password,
     ) => {
     let sucess = false;
     let response = null;
     let result = null;
 
-    await api.post('/accounts/user', {
-        
+    await api.post('/accounts/user/', {
+        "name_user": user_name,
+        "email": email,
+        "password": password
+    })
+    .then(res => {
+        sucess = true;
+        response = res;
+        result = res.data
+    })
+    .catch(res => {
+        sucess = false;
+        response = res
+    })
+
+    return ({
+        sucess: sucess,
+        response: response,
+        result: result
     })
 }
